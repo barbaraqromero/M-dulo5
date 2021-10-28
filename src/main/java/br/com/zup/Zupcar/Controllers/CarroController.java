d49302eabd675a65dfd7e669dea9d55677afc3f0
Bárbara Romero <bqromero@gmail.com>
2021-10-28 16:06:31 -0300

    Criar método para atualizar o carro

--- src/main/java/br/com/zup/Zupcar/Controllers/CarroController.java
+++ src/main/java/br/com/zup/Zupcar/Controllers/CarroController.java
@@ -28,13 +28,27 @@ public class CarroController {
   @GetMapping("/{nomeDoCarro}")
   public CarroDTO exibirCarro(@PathVariable String nomeDoCarro) {
     for (CarroDTO referencia : frota) {
-      if (referencia.getModelo().equals(nomeDoCarro)){
+      if (referencia.getModelo().equalsIgnoreCase(nomeDoCarro)) {
         return referencia;
       }
 
 
     }
     throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+  }
+
+  @PutMapping("/{nomeDoCarro}")
+  public CarroDTO atualizarCarro(@PathVariable String nomeDoCarro, @RequestBody CarroDTO carroDTO) {
+    for (CarroDTO referencia : frota){
+      if (referencia.getModelo().equalsIgnoreCase(nomeDoCarro)){
+        referencia.setCor(referencia.getCor());
+        referencia.setMotor(referencia.getMotor());
+        referencia.setAno(referencia.getAno());
+        return referencia;
+      }
+    }
+    throw  new ResponseStatusException(HttpStatus.NOT_FOUND);
   }
 }
 
